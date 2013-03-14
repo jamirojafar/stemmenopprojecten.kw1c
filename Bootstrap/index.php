e2562d5df1bbb4437bb8fa1ad80b30948e3acfc8
Dirk Groenen <dirk_groenen@live.nl>
2013-03-14 11:33:28 +0100

    Bootstrap -> lichte styling

---
+++ Bootstrap/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>KW1C - Community Week</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/docs.css" rel="stylesheet">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+	<link rel="shortcut icon" href="assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <!-- Navbar
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">KW1C</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active">
+                <a href="./index.php">Categorie 1</a>
+              </li>
+			  <li class="active">
+                <a href="./index.php">Categorie 2</a>
+              </li>
+			  <li class="active">
+                <a href="./index.php">Categorie 3</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="container">
+  <div class="changes">
+    <h1>What's different?</h1>
+    <p class="marketing-byline">How can a fantastic library become any better? Check it out.</p>
+    <div class="row">
+      <div class="span3">
+        <h3><a href="./javascript.html#typeahead">Typeahead</a></h3>
+        <p>With the improved typeahead, it's easy to fetch the data using AJAX.</p>
+        <p>It support key/value pairs (objects) next to normal arrays. Also you can convert a <code>&lt;select&gt;</code> to a typeahead.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./javascript.html#inputmask">Input mask</a></h3>
+        <p>Force the user to enter data conform a specific format.</p>
+        <p>Unlike validation, the user can't enter any other key than the ones specified by the mask.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./javascript.html#rowlink">Row link</a></h3>
+        <p>Don't like when can only click on one column in a table?</p>
+        <p>Row link is a simple javascript plugin that makes the whole row clickable.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./javascript.html#fileupload">File upload</a></h3>
+        <p>With the file upload plugin you can create visually appealing file or image upload widgets.</p>
+        <p>The image upload widget shows a preview of the selected image even before it's uploaded.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="span3">
+        <h3><a href="./base-css.html#iconic">Iconic icons</a></h3>
+        <p>The iconic font set adds 170 new icons to Bootstrap. These icons can be used with buttons, menus, links, etc.</p>
+        <p>Because these icons are a font, they can be styled just like text.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./components.html#page-alerts">Page alerts</a></h3>
+        <p>Know those nice little alerts after you save your profile? With .page-alert the alert will be shown on top op your page.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./scaffolding.html#semi-fluid-layout">Semi-fluid layout</a></h3>
+        <p>Get the best of both worlds with a semi-fluid layout. It acts as a fixed layout for big screens. For smaller screens, it behaves like a fluid layout.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./scaffolding.html#row-desktop">Desktop rows</a></h3>
+        <p>By default columns of a row are show next to each other for tablets and desktops and not for phones. A desktop row displays acts like a row for desktops only and not for tablets or phones.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="span3">
+        <h3><a href="./base-css.html#action-links">Action links</a></h3>
+        <p>Sometimes a button will pull to much attention to an action. In those cases, you can use action links instead.</p>
+        <p>Action links look distinctly different than normal links.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./base-css.html#forms">Form elements</a></h3>
+        <p>Go beyond uneditable input and create a full uneditable form, with disabled and uneditable elements.</p>
+        <p>Add .editor to a textarea to create a larger field for content editing.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./base-css.html#forms">Form styles</a></h3>
+        <p>Combine horizontal and vertical forms. Using wells in horizontal forms now looks clean.</p>
+        <p>Use smaller labels to fit more input fields on one screen.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./components.html#navs">Tabbable</a></h3>
+        <p>Tabs work better than ever. You now have a bordered tabbable available. Also, a container can be turned into a tabbable.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="span3">
+        <h3><a href="./customize.html">Customize</a></h3>
+        <p>Each customized bootstrap.zip contains a `custom.json` file, which can be used to use the same packages and variables when upgrading.</p>
+        <p>Automatically includes the CSS for jQuery plugins.</p>
+      </div>
+      <div class="span3">
+        <h3><a href="./javascript.html">Download js plugins</a></h3>
+        <p>Download a jQuery plugin directly from the javascript docs, including required CSS.</p>
+      </div>
+      <div class="span3">
+        <h3>More</h3>
+        <ul>
+          <li>Action area for header <code>.header-actions</code></li>
+          <li>Action area for tables <code>.table-actions</code></li>
+          <li>Fixed margin for modal forms</li>
+        </ul>
+      </div>
+    </div><!--/row-->
+  </div>
+
+  <hr class="soften">
+
+  <div class="marketing">
+    <h1>Introducing Bootstrap.</h1>
+    <p class="marketing-byline">Need reasons to love Bootstrap? Look no further.</p>
+
+    <div class="row-fluid">
+      <div class="span4">
+        <h2>By nerds, for nerds.</h2>
+        <p>Built at Twitter by <a href="http://twitter.com/mdo">@mdo</a> and <a href="http://twitter.com/fat">@fat</a>, Bootstrap utilizes <a href="http://lesscss.org">LESS CSS</a>, is compiled via <a href="http://nodejs.org">Node</a>, and is managed through <a href="http://github.com">GitHub</a> to help nerds do awesome stuff on the web.</p>
+      </div>
+      <div class="span4">
+        <h2>Made for everyone.</h2>
+        <p>Bootstrap was made to not only look and behave great in the latest desktop browsers (as well as IE7!), but in tablet and smartphone browsers via <a href="./scaffolding.html#responsive">responsive CSS</a> as well.</p>
+      </div>
+      <div class="span4">
+        <h2>Packed with features.</h2>
+        <p>A 12-column responsive <a href="./scaffolding.html#gridSystem">grid</a>, dozens of components, <a href="./javascript.html">JavaScript plugins</a>, typography, form controls, and even a <a href="./customize.html">web-based Customizer</a> to make Bootstrap your own.</p>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+
+    <!-- Footer
+    ================================================== -->
+    <footer class="footer">
+      <div class="container">
+        <p>Ruimte voor de namen van de beste ontwikkelaars van het Koning Willem I College</p>
+      </div>
+    </footer>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/holder/holder.js"></script>
+    <script src="assets/js/google-code-prettify/prettify.js"></script>
+
+  </body>
+</html>
