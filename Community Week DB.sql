596d69ad258e5e56dd89ab0cbfdc4922abcc94ba
Dirk Groenen <dirk_groenen@live.nl>
2013-03-14 09:11:58 +0100

    Update varchar wachtwoord naar varchar(35)
    Als beveiliging een hash (md5) heeft ongeveer 32 characters nodig

--- Community Week DB.sql
+++ Community Week DB.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS `beheerders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gebruikersnaam` varchar(20) NOT NULL,
-  `wachtwoord` varchar(16) NOT NULL,
+  `wachtwoord` varchar(35) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
