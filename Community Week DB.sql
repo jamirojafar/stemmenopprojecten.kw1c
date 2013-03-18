cd43679a0784567999e91ae9de958dbb6247109f
Mitchel van Amstel <m.a.h.vanamstel@veolic.nl>
2013-03-18 14:35:43 +0100

    Database structuur geupdate

--- Community Week DB.sql
+++ Community Week DB.sql
@@ -12,42 +12,15 @@ CREATE TABLE IF NOT EXISTS `beheerders` (
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `doelen`
+-- Tabelstructuur voor tabel `items`
 --
 
-CREATE TABLE IF NOT EXISTS `doelen` (
+CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `naam` varchar(75) NOT NULL,
   `beschrijving` longtext NOT NULL,
   `afbeelding_bestandsnaam` varchar(25) NOT NULL DEFAULT 'leeg.png',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `kranten`
---
-
-CREATE TABLE IF NOT EXISTS `kranten` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `naam` varchar(75) NOT NULL,
-  `beschrijving` longtext NOT NULL,
-  `afbeelding_bestandsnaam` varchar(25) NOT NULL DEFAULT 'geen.png',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `projecten`
---
-
-CREATE TABLE IF NOT EXISTS `projecten` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `naam` varchar(75) NOT NULL,
-  `beschrijving` longtext NOT NULL,
-  `afbeelding_bestandsnaam` varchar(25) NOT NULL DEFAULT 'geen.png',
+  `categorie` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -59,14 +32,10 @@ CREATE TABLE IF NOT EXISTS `projecten` (
 
 CREATE TABLE IF NOT EXISTS `stemmen` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(111) NOT NULL,
-  `krant_id` int(11) NOT NULL,
-  `doel_id` int(11) NOT NULL,
+  `item_id` int(111) NOT NULL,
   `ip_adres` varchar(25) NOT NULL DEFAULT '0.0.0.0',
   PRIMARY KEY (`id`),
-  KEY `project_id` (`project_id`),
-  KEY `krant_id` (`krant_id`),
-  KEY `doel_id` (`doel_id`)
+  KEY `item_id` (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -77,6 +46,4 @@ CREATE TABLE IF NOT EXISTS `stemmen` (
 -- Beperkingen voor tabel `stemmen`
 --
 ALTER TABLE `stemmen`
-  ADD CONSTRAINT `stemmen_ibfk_3` FOREIGN KEY (`doel_id`) REFERENCES `doelen` (`id`),
-  ADD CONSTRAINT `stemmen_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projecten` (`id`),
-  ADD CONSTRAINT `stemmen_ibfk_2` FOREIGN KEY (`krant_id`) REFERENCES `kranten` (`id`);
+  ADD CONSTRAINT `stemmen_ibfk_3` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
