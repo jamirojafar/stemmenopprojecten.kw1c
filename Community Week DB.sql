d8546467ce7f85391029639eb92e18dd25630b9e
Mitchel van Amstel <m.a.h.vanamstel@veolic.nl>
2013-03-18 14:42:33 +0100

    Database structuur opnieuw geupdate

--- Community Week DB.sql
+++ Community Week DB.sql
@@ -1,19 +1,11 @@
---
--- Tabelstructuur voor tabel `beheerders`
---
+CREATE DATABASE `stemmen`
 
 CREATE TABLE IF NOT EXISTS `beheerders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gebruikersnaam` varchar(20) NOT NULL,
-  `wachtwoord` varchar(35) NOT NULL,
+  `wachtwoord` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `items`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -22,28 +14,16 @@ CREATE TABLE IF NOT EXISTS `items` (
   `afbeelding_bestandsnaam` varchar(25) NOT NULL DEFAULT 'leeg.png',
   `categorie` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `stemmen`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `stemmen` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(111) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ip_adres` varchar(25) NOT NULL DEFAULT '0.0.0.0',
   PRIMARY KEY (`id`),
   KEY `item_id` (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Beperkingen voor gedumpte tabellen
---
-
---
--- Beperkingen voor tabel `stemmen`
---
 ALTER TABLE `stemmen`
   ADD CONSTRAINT `stemmen_ibfk_3` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
